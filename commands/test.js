b5241b7c62410ea93a228110a84dcb43a6ea25b8
Andreas Grafen <35840154+andreasgrafen@users.noreply.github.com>
2019-01-16 12:40:04 +0100

    fixed typos

--- commands/test.js
+++ commands/test.js
@@ -4,11 +4,11 @@
  *  Every Command has to export a function called execute.
  *  This function takes two parameters:
  *  – the resolve function from the command handler
- *  – th arguments provided by the user (optional)
+ *  – the arguments provided by the user (optional)
  *
- *  The execute function has to return an object oncluding:
+ *  The execute function has to return an object including:
  *  – the type of response; text, image, gif, audio, video
- *  – the content the bot should return to th user
+ *  – the content the bot returns to th user
  *  The return statement has to be wrapped inside the resolve function
  */
 
